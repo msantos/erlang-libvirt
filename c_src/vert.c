daac6ecbd3e23c77b62b73338bafdb3e81921c79
Michael Santos <michael.santos@gmail.com>
2012-03-13 09:09:40 -0400

    Fix memory leak on error condition

--- c_src/vert.c
+++ c_src/vert.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2010-2011, Michael Santos <michael.santos@gmail.com>
+/* Copyright (c) 2010-2012, Michael Santos <michael.santos@gmail.com>
  * All rights reserved.
  * 
  * Redistribution and use in source and binary forms, with or without
@@ -220,6 +220,7 @@ vert_cast(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[])
 
     if (write(state->fd[VERT_WRITE], &cmd, sizeof(VERT_CAST)) < 0) {
         res = error_errno(env, errno);
+        enif_free(cmd.argv);
         goto OUT;
     }
 
